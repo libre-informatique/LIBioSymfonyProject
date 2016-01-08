5a1f05ac3f7afbe00adc3dd13dbd26480ad9e219
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-01-08 16:32:13 +0100

    Added a test controller & view

--- src/Librinfo/LibioBundle/Controller/DefaultController.php
+++ src/Librinfo/LibioBundle/Controller/DefaultController.php
@@ -10,4 +10,19 @@ class DefaultController extends Controller
     {
         return $this->render('LibrinfoLibioBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    public function testAction($name)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $contact = $this->getDoctrine()
+            ->getRepository('LibrinfoCRMBundle:Contact')
+            ->find('b419cf55-9ed6-4274-9cb6-b3d6f45c61d2');
+
+        $indexes = $contact->getSearchIndexes();
+        dump(count($indexes));
+
+
+        return $this->render('LibrinfoLibioBundle:Default:test.html.twig', array('name' => $name));
+    }
 }
