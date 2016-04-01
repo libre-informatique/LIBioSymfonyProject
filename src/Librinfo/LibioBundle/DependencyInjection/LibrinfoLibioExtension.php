e592e6b357b52f7b5367c0c91b6ca1ea217f0cb1
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-04-01 16:34:37 +0200

    Created Family and Genus entities and admins

--- src/Librinfo/LibioBundle/DependencyInjection/LibrinfoLibioExtension.php
+++ src/Librinfo/LibioBundle/DependencyInjection/LibrinfoLibioExtension.php
@@ -6,13 +6,15 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Librinfo\CoreBundle\DependencyInjection\LibrinfoCoreExtension;
+use Librinfo\CoreBundle\DependencyInjection\DefaultParameters;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class LibrinfoLibioExtension extends Extension
+class LibrinfoLibioExtension extends LibrinfoCoreExtension
 {
     /**
      * {@inheritdoc}
@@ -24,5 +26,13 @@ class LibrinfoLibioExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('admin.yml');
+
+        if ($container->getParameter('kernel.environment') == 'test')
+        {
+            $loader->load('datafixtures.yml');
+        }
+
+        $this->mergeParameter('librinfo', $container, __DIR__ . '/../Resources/config');
     }
 }
