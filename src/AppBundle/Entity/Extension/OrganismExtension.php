ad5fa8d29f3b8af38d2069838d58d2b2fea0bd29
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-09-30 10:32:44 +0200

    Refactored OrganismExtension

--- src/AppBundle/Entity/Extension/OrganismExtension.php
+++ src/AppBundle/Entity/Extension/OrganismExtension.php
@@ -5,6 +5,5 @@ namespace AppBundle\Entity\Extension;
 trait OrganismExtension
 {
     use \Librinfo\EmailCRMBundle\Entity\Traits\HasEmailMessages;
-    use \Librinfo\SeedBatchBundle\Entity\Traits\Inverse\HasSeedBatches;
-    use \Librinfo\SeedBatchBundle\Entity\Traits\Inverse\HasPlots;
+    use \Librinfo\SeedBatchBundle\Entity\Traits\OrganismExtension;
 }
