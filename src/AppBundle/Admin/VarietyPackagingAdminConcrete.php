0365c55b551b2173ee4564108ad4e12a1121ba2e
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-11-08 17:06:03 +0100

    Added VarietyPackaging Admin

---
+++ src/AppBundle/Admin/VarietyPackagingAdminConcrete.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the BLAST package <http://blast.libre-informatique.fr>.
+ *
+ * Copyright (C) 2015-2016 Libre Informatique
+ *
+ * This file is licenced under the GNU GPL v3.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Admin;
+
+use Librinfo\CoreBundle\Admin\Traits\Base;
+
+class VarietyPackagingAdminConcrete extends VarietyPackagingAdmin
+{
+    use Base;
+
+}
+
