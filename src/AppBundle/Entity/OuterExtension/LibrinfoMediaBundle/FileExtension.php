fbfacbd5284231193b06580e51b8f1023710634d
Romain <romain.sanchez@libre-informatique.fr>
2016-11-21 15:30:14 +0100

    refactoring

---
+++ src/AppBundle/Entity/OuterExtension/LibrinfoMediaBundle/FileExtension.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AppBundle\Entity\OuterExtension\LibrinfoMediaBundle;
+
+trait FileExtension
+{
+    use \Librinfo\EmailBundle\Entity\OuterExtension\HasEmail;
+    use \Librinfo\VarietiesBundle\Entity\OuterExtension\HasVariety;
+}
