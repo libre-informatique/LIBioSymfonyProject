ba062d3cfcdc0ee5ef4cfdfece1eb4ebafa454ce
Romain <romain.sanchez@libre-informatique.fr>
2016-11-22 16:43:32 +0100

    Removed unnecessary override of LibrinfoCoreExtension::load()

--- src/AppBundle/DependencyInjection/AppExtension.php
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -3,10 +3,6 @@
 namespace AppBundle\DependencyInjection;
 
 use Librinfo\CoreBundle\DependencyInjection\LibrinfoCoreExtension;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
-
 /**
  * This is the class that loads and manages your bundle configuration
  *
@@ -14,24 +10,4 @@ use Symfony\Component\DependencyInjection\Loader;
  */
 class AppExtension extends LibrinfoCoreExtension
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function load(array $configs, ContainerBuilder $container)
-    {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
-        $loader->load('admin.yml');
-        //$loader->load('config.yml');
-
-        if ($container->getParameter('kernel.environment') == 'test')
-        {
-            $loader->load('datafixtures.yml');
-        }
-
-        $this->mergeParameter('librinfo', $container, __DIR__ . '/../Resources/config');
-    }
 }
