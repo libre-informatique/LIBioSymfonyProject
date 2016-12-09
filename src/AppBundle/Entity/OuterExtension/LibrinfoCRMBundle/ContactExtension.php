d62bf6c5b782ef8376eda7ddf8fa2639bdba04a7
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-12-09 11:02:19 +0100

    Added Traceable trait in some CRM outer extensions + some licence headers

--- src/AppBundle/Entity/OuterExtension/LibrinfoCRMBundle/ContactExtension.php
+++ src/AppBundle/Entity/OuterExtension/LibrinfoCRMBundle/ContactExtension.php
@@ -13,4 +13,5 @@ namespace AppBundle\Entity\OuterExtension\LibrinfoCRMBundle;
 trait ContactExtension
 {
     use \Librinfo\EmailCRMBundle\Entity\OuterExtension\HasEmailMessages;
+    use \Librinfo\UserBundle\Entity\Traits\Traceable;
 }
