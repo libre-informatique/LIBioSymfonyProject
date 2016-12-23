cb9b9e2cf14dd05e803113d5b799b0d13b36fd94
Romain <romain.sanchez@libre-informatique.fr>
2016-12-23 10:14:36 +0100

    Removed LibrinfoMediabundle:File unneeded outer extensions

--- src/AppBundle/Entity/OuterExtension/LibrinfoMediaBundle/FileExtension.php
+++ src/AppBundle/Entity/OuterExtension/LibrinfoMediaBundle/FileExtension.php
@@ -4,6 +4,4 @@ namespace AppBundle\Entity\OuterExtension\LibrinfoMediaBundle;
 
 trait FileExtension
 {
-    use \Librinfo\EmailBundle\Entity\OuterExtension\HasEmail;
-    use \Librinfo\VarietiesBundle\Entity\OuterExtension\HasVariety;
 }
