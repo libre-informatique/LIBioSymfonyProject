56ef8b3074459221aacfb5b160608dbec8e0d9ee
Romain <romain.sanchez@libre-informatique.fr>
2017-01-31 11:52:14 +0100

    Fixed LibrinfoEcommerceBundle OrganismExtensionInterface alias

--- src/AppBundle/Entity/OuterExtension/LibrinfoCRMBundle/OrganismExtensionInterface.php
+++ src/AppBundle/Entity/OuterExtension/LibrinfoCRMBundle/OrganismExtensionInterface.php
@@ -2,10 +2,10 @@
 
 namespace AppBundle\Entity\OuterExtension\LibrinfoCRMBundle;
 
-use Librinfo\EcommerceBundle\Entity\OuterExtension\LibrinfoCRMBundle\OrganismExtensionInterface as EcommerceContactExtension
+use Librinfo\EcommerceBundle\Entity\OuterExtension\LibrinfoCRMBundle\OrganismExtensionInterface as EcommerceOrganismExtension
 ;
 
-interface OrganismExtensionInterface extends EcommerceContactExtension
+interface OrganismExtensionInterface extends EcommerceOrganismExtension
 {
     
 }
