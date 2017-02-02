fd3b2b5be7686e21490ac42a96cc271965352ca1
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2017-02-02 17:52:25 +0100

    Added Variety / Packaging (ProductOptionValue) many-to-many relationship

--- src/AppBundle/Entity/OuterExtension/LibrinfoVarietiesBundle/VarietyExtension.php
+++ src/AppBundle/Entity/OuterExtension/LibrinfoVarietiesBundle/VarietyExtension.php
@@ -10,6 +10,9 @@
 
 namespace AppBundle\Entity\OuterExtension\LibrinfoVarietiesBundle;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Librinfo\EcommerceBundle\Entity\ProductOptionValue;
+
 /**
  * @author Romain SANCHEZ <romain.sanchez@libre-informatique.fr>
  */
@@ -20,4 +23,45 @@ trait VarietyExtension
     use \Librinfo\EcommerceBundle\Entity\OuterExtension\HasProducts;
     use \Librinfo\UserBundle\Entity\Traits\Traceable;
     use \AppBundle\Entity\OuterExtension\HasChannelDescriptions;
+
+    /**
+     * @var ArrayCollection
+     */
+    private $packagings;
+
+    public function initPackagings()
+    {
+        $this->packagings = new ArrayCollection();
+    }
+
+    /**
+     * @param ProductOptionValue $packaging
+     * @return self
+     */
+    public function addPackaging(ProductOptionValue $packaging)
+    {
+        $this->packagings->add($packaging);
+
+//        $this->setOwningSideRelation($packaging);
+
+        return $this;
+    }
+
+    /**
+     * @param ProductOptionValue $packaging
+     * @return self
+     */
+    public function removePackaging(ProductOptionValue $packaging)
+    {
+        $this->packagings->removeElement($packaging);
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getPackagings()
+    {
+        return $this->packagings;
+    }
 }
