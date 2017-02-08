55a06a50bce0479e193a790e851e07415cee53e3
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2017-02-08 11:26:41 +0100

    Added licences to some JS files

--- app/themes/LisemTheme/SyliusShopBundle/Gulpfile.js
+++ app/themes/LisemTheme/SyliusShopBundle/Gulpfile.js
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) Paweł Jędrzejewski
+ * Copyright (C) 2015-2016 Libre Informatique
+ *
+ * This file is licenced under the GNU GPL v3.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 // original file: vendor/sylius/sylius/src/Sylius/Bundle/ShopBundle/Gulpfile.js
 
 var concat = require('gulp-concat');
--- app/themes/LisemTheme/SyliusShopBundle/Resources/private/js/semantic-ui-api-actions.js
+++ app/themes/LisemTheme/SyliusShopBundle/Resources/private/js/semantic-ui-api-actions.js
@@ -1,8 +1,8 @@
 /*
- * This file is part of the Sylius package.
+ * Copyright (C) Paweł Jędrzejewski
+ * Copyright (C) 2015-2016 Libre Informatique
  *
- * (c) Paweł Jędrzejewski
- *
+ * This file is licenced under the GNU GPL v3.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
