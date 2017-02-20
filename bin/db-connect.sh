8022230779f393e046896d06f9c30213ecfee755
Romain <romain.sanchez@libre-informatique.fr>
2017-02-20 16:06:29 +0100

    Code clean up

--- bin/db-connect.sh
+++ bin/db-connect.sh
@@ -22,18 +22,9 @@ export PGHOST=`grep database_host app/config/parameters.yml | grep -v '^#' | sed
 [ -z "$PGDATABASE" ] && \
 export PGDATABASE=`grep database_name app/config/parameters.yml | grep -v '^#' | sed "s/\s*database_name:[^a-Z^0-9]*\(.*\)$/\1/g" | head -n$LINE | tail -n1`
 
-# host & db
-#CONN=`grep dsn app/config/parameters.yml | grep -v '^#' | sed "s/\s*dsn:\s*'\(.*\):host=\(.*\);dbname=\(\w*\).*/\1:\2:\3/g" | head -n$LINE | tail -n1`
-#if [ "`echo $CONN | cut -d : -f 1`" = 'pgsql' ]; then
-#  CMD=psql
-#fi
-
-# host
-#export PGHOST=`echo $CONN | cut -d : -f 2`
-
 # db
 #export PGDATABASE=`echo $CONN | cut -d : -f 3`
-echo "$PGPASSWORD"
+
 echo "+------------------------------------------------+"
 echo "| Host    : $PGHOST"
 echo "| Database: $PGDATABASE"
