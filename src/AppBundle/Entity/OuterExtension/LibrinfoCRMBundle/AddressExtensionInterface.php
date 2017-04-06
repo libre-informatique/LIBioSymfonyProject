b6445bf9c56d63a4c4cf829b524ea3706b462ee6
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2017-04-06 14:39:17 +0200

    Minor (licences)

--- src/AppBundle/Entity/OuterExtension/LibrinfoCRMBundle/AddressExtensionInterface.php
+++ src/AppBundle/Entity/OuterExtension/LibrinfoCRMBundle/AddressExtensionInterface.php
@@ -1,12 +1,20 @@
 <?php
 
+/*
+ * Copyright (C) 2015-2017 Libre Informatique
+ *
+ * This file is licenced under the GNU GPL v3.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AppBundle\Entity\OuterExtension\LibrinfoCRMBundle;
 
-use Librinfo\EcommerceBundle\Entity\OuterExtension\LibrinfoCRMBundle\AddressExtensionInterface 
+use Librinfo\EcommerceBundle\Entity\OuterExtension\LibrinfoCRMBundle\AddressExtensionInterface
     as EcommerceAddressExtension
 ;
 
 interface AddressExtensionInterface extends EcommerceAddressExtension
 {
-    
+
 }
--- src/AppBundle/Entity/OuterExtension/LibrinfoCRMBundle/OrganismExtensionInterface.php
+++ src/AppBundle/Entity/OuterExtension/LibrinfoCRMBundle/OrganismExtensionInterface.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * Copyright (C) 2015-2017 Libre Informatique
+ *
+ * This file is licenced under the GNU GPL v3.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AppBundle\Entity\OuterExtension\LibrinfoCRMBundle;
 
 use Librinfo\EcommerceBundle\Entity\OuterExtension\LibrinfoCRMBundle\OrganismExtensionInterface as EcommerceOrganismExtension
@@ -7,5 +15,5 @@ use Librinfo\EcommerceBundle\Entity\OuterExtension\LibrinfoCRMBundle\OrganismExt
 
 interface OrganismExtensionInterface extends EcommerceOrganismExtension
 {
-    
+
 }
