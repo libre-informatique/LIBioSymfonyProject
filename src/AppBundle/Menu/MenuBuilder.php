c61b8d18cff0a643bd4fc0686e1d30c793c6ec58
PapsOu <vivien.frasca@libre-informatique.fr>
2017-05-03 15:49:06 +0200

    Added tax rates / categories management in sonata admin

--- src/AppBundle/Menu/MenuBuilder.php
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -91,8 +91,8 @@ class MenuBuilder
         $submenu->addChild('libio.menu_label.customer_group', ['route' => 'admin_librinfo_ecommerce_customergroup_list']);
         $submenu->addChild('libio.menu_label.payment_methods_list', ['route' => 'admin_librinfo_ecommerce_payment_method_list']);
         $submenu->addChild('libio.menu_label.shipping_methods_list', ['route' => 'libio_not_implemented']);
-        $submenu->addChild('libio.menu_label.tax_categories_list', ['route' => 'libio_not_implemented']);
-        $submenu->addChild('libio.menu_label.tax_rates_list', ['route' => 'libio_not_implemented']);
+        $submenu->addChild('libio.menu_label.tax_categories_list', ['route' => 'admin_librinfo_ecommerce_taxcategory_list']);
+        $submenu->addChild('libio.menu_label.tax_rates_list', ['route' => 'admin_librinfo_ecommerce_taxrate_list']);
 
         return $menu;
     }
