ca7d366f62d7233e21e6b4d9d4b617dd5980ec96
François GERBES <francois.gerbes@libre-informatique.fr>
2017-06-14 14:46:43 +0200

    Add Exec Right

---
+++ bin/ci-scripts/before_script_test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+set -ev
+
+# database creation
+
+bin/console doctrine:database:create --if-not-exists --no-interaction
+bin/console doctrine:schema:create --no-interaction
+#bin/console doctrine:schema:update --force --no-interaction
+bin/console doctrine:schema:validate --no-interaction
+
+# lisem
+bin/console lisem:install:setup --with-samples
+
+# npm
+npm install
+npm run gulp
+
+# start server
+bin/console server:start --no-interaction
