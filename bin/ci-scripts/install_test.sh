f858b53e8847cb2ce92c83250c11833dbae770d1
François GERBES <francois.gerbes@libre-informatique.fr>
2017-06-14 13:54:29 +0200

    Adaptation for lisem
    Remove update, as we already have a composer.lock and want to test it.
    Add require for codecept (for selenium)

--- bin/ci-scripts/install_test.sh
+++ bin/ci-scripts/install_test.sh
@@ -8,8 +8,7 @@ echo "memory_limit=-1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/t
 
 composer install --no-interaction --prefer-dist
 composer require --no-interaction --dev phpunit/phpunit 
-composer update --prefer-dist --no-interaction --prefer-stable
-
+composer require --no-interaction --dev "codeception/codeception:*"
 
 # Coveralls client install
 wget https://github.com/satooshi/php-coveralls/releases/download/v1.0.1/coveralls.phar --output-document="${HOME}/bin/coveralls"
