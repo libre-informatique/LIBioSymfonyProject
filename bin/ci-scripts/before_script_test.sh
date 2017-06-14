f149083d0f82b15e856d54c5df18577bdf4a5d2d
François GERBES <francois.gerbes@libre-informatique.fr>
2017-06-14 18:21:00 +0200

    Add Create

--- bin/ci-scripts/before_script_test.sh
+++ bin/ci-scripts/before_script_test.sh
@@ -4,9 +4,9 @@ set -ev
 # database creation
 
 bin/console doctrine:database:create --if-not-exists --no-interaction
-#bin/console doctrine:schema:create --no-interaction
+bin/console doctrine:schema:create --no-interaction
 #bin/console doctrine:schema:update --force --no-interaction
-bin/console doctrine:schema:validate --no-interaction
+#bin/console doctrine:schema:validate --no-interaction
 
 # lisem
 bin/console lisem:install:setup --with-samples
