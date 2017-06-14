a8d259cb35e25005fca95f3dec222d9493e8c723
François GERBES <francois.gerbes@libre-informatique.fr>
2017-06-14 16:08:10 +0200

    add update to be able to well validate

--- bin/ci-scripts/before_script_test.sh
+++ bin/ci-scripts/before_script_test.sh
@@ -5,7 +5,7 @@ set -ev
 
 bin/console doctrine:database:create --if-not-exists --no-interaction
 bin/console doctrine:schema:create --no-interaction
-#bin/console doctrine:schema:update --force --no-interaction
+bin/console doctrine:schema:update --force --no-interaction
 bin/console doctrine:schema:validate --no-interaction
 
 # lisem
