3c74457fe96b9e8b9b4f1b552436b9ae3afd4bb8
François GERBES <francois.gerbes@libre-informatique.fr>
2017-06-14 16:44:47 +0200

    Remove useless and bugged command

--- bin/ci-scripts/before_script_test.sh
+++ bin/ci-scripts/before_script_test.sh
@@ -4,8 +4,8 @@ set -ev
 # database creation
 
 bin/console doctrine:database:create --if-not-exists --no-interaction
-bin/console doctrine:schema:create --no-interaction
-bin/console doctrine:schema:update --force --no-interaction
+#bin/console doctrine:schema:create --no-interaction
+#bin/console doctrine:schema:update --force --no-interaction
 bin/console doctrine:schema:validate --no-interaction
 
 # lisem
