3110230e15caef9cce4814053c84425f91577d64
François GERBES <francois.gerbes@libre-informatique.fr>
2017-06-15 13:04:03 +0200

    Try to supress spam output for nvm install

--- bin/ci-scripts/before_script_test.sh
+++ bin/ci-scripts/before_script_test.sh
@@ -19,9 +19,9 @@ rm -rf $HOME/.nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 
-set +x
+set +ev
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-set -x
+set -ev
 
 # install node 4.2.6
 nvm install 4.2.6
