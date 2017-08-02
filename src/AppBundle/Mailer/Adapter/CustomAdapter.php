b220fc5dc686930420f12da8cd4243efd6c06536
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2017-08-02 17:08:38 +0200

    Fix CS

--- src/AppBundle/Mailer/Adapter/CustomAdapter.php
+++ src/AppBundle/Mailer/Adapter/CustomAdapter.php
@@ -1,10 +1,12 @@
 <?php
 
 /*
+ * This file is part of the Lisem Project.
+ *
  * Copyright (C) 2015-2017 Libre Informatique
  *
  * This file is licenced under the GNU GPL v3.
- * For the full copyright and license information, please view the LICENSE
+ * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
  */
 
@@ -13,7 +15,7 @@ namespace AppBundle\Mailer\Adapter;
 use Sylius\Bundle\MailerBundle\Sender\Adapter\SwiftMailerAdapter;
 
 /**
- * This class extends the default Sylius email sender adapter to allow self-signed certificates
+ * This class extends the default Sylius email sender adapter to allow self-signed certificates.
  *
  * @author Marcos Bezerra de Menezes <marcos.bezerra@libre-informatique.fr>
  */
