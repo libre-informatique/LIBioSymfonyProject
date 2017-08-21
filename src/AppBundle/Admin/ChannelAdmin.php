2627317e243b1adfd7b2d069a9d9409be4a194d4
PapsOu <vivien.frasca@libre-informatique.fr>
2017-08-21 18:06:42 +0200

    Stylefix

--- src/AppBundle/Admin/ChannelAdmin.php
+++ src/AppBundle/Admin/ChannelAdmin.php
@@ -1,23 +1,21 @@
 <?php
 
 /*
- * This file is part of the Blast Project package.
+ * This file is part of the Lisem Project.
  *
  * Copyright (C) 2015-2017 Libre Informatique
  *
- * This file is licenced under the GNU LGPL v3.
+ * This file is licenced under the GNU GPL v3.
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
  */
 
 namespace AppBundle\Admin;
 
-use Blast\CoreBundle\Admin\CoreAdmin;
 use Librinfo\EcommerceBundle\Admin\ChannelAdmin as BaseChannelAdmin;
 
 class ChannelAdmin extends BaseChannelAdmin
 {
-
     public function createQuery($context = 'list')
     {
         $query = parent::createQuery($context);
