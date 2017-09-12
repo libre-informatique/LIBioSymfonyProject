eddd09bb2166cb8ce17064acc9532bce92e52e2a
PapsOu <vivien.frasca@libre-informatique.fr>
2017-09-12 12:31:27 +0200

    Fix theme layout broken

--- src/AppBundle/DependencyInjection/AppExtension.php
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -13,6 +13,7 @@
 namespace AppBundle\DependencyInjection;
 
 use Blast\CoreBundle\DependencyInjection\BlastCoreExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
  * This is the class that loads and manages your bundle configuration.
@@ -21,4 +22,15 @@ use Blast\CoreBundle\DependencyInjection\BlastCoreExtension;
  */
 class AppExtension extends BlastCoreExtension
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        parent::load($configs,$container);
+        // Loads Librinfo Decorator sonata layout insteadof blastcore layout
+        $sonataTemplates = $container->getParameter('sonata.admin.configuration.templates');
+        $sonataTemplates['layout'] = 'LibrinfoDecoratorBundle:Admin:layout.html.twig';
+        $container->setParameter('sonata.admin.configuration.templates', $sonataTemplates);
+    }
 }
