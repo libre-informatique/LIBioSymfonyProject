d6b4fc8d931daa5f53ba5a6269a92e8735d68b60
François GERBES <francois.gerbes@libre-informatique.fr>
2017-10-06 17:45:23 +0200

    Try to go on show and edit page

--- tests/acceptance/042_AutoTestCept.php
+++ tests/acceptance/042_AutoTestCept.php
@@ -13,6 +13,7 @@
 $I = new WebGuy($scenario);
 $I->wantTo('Test All Route');
 
+
 function doLogin($webGuy)
 {
     $webGuy->wantTo('Login');
@@ -23,17 +24,52 @@ function doLogin($webGuy)
     $webGuy->click("//button[@type='submit']");
 }
 
+function checkPage($webGuy, $urlPage, &$linkList)
+{
+    $webGuy->wantTo('Test Route: ' . $urlPage);
+    $webGuy->amOnPage($urlPage);
+    $webGuy->waitForText('Libre', 10); // secs
+    $webGuy->dontSee('Stack Trace'); /* :) :) we hope so */
+
+
+    $allLink = $webGuy->grabMultiple('a', 'href');
+    $allShow =  preg_grep('/show$/', $allLink);
+    $allEdit =  preg_grep('/edit$/', $allLink);
+    
+    /* @todo : factorize this + should use a random (not always the first one)  */
+    $cpt = 3;
+    foreach ($allShow as $oneShow) {
+        array_push($linkList, $oneShow);
+        if ($cpt-- == 0) {
+            break;
+        }
+    }
+    
+    $cpt = 3;
+    foreach ($allEdit as $oneEdit) {
+        array_push($linkList, $oneEdit);
+        if ($cpt-- == 0) {
+            break;
+        }
+    }
+    //dump($linkList);
+    //return $linkList;
+}
+
+$allLink = array ();
 doLogin($I);
 
 //## Get Some Symfony Service ####
 $curRouter = $I->grabServiceFromContainer('router');
-$curTranslator = $I->grabServiceFromContainer('translator');
-$curCatalogue = $curTranslator->getCatalogue();
+//$curTranslator = $I->grabServiceFromContainer('translator');
+//$curCatalogue = $curTranslator->getCatalogue();
 /* As we don't use the good locale ... */
-if (empty($curCatalogue->all('messages'))) {
-    $curCatalogue = $curCatalogue->getFallbackCatalogue();
-}
-$curMessage = $curCatalogue->all('messages');
+// if (empty($curCatalogue->all('messages'))) {
+//     $curCatalogue = $curCatalogue->getFallbackCatalogue();
+// }
+// $curMessage = $curCatalogue->all('messages');
+
+
 
 foreach ($curRouter->getRouteCollection() as $curRoute) {
     $routePath = $curRoute->getPath();
@@ -44,14 +80,16 @@ foreach ($curRouter->getRouteCollection() as $curRoute) {
     /* Select only usefull route (or not) */
     if (preg_match('/lisem|librinfo/', $routePath)
         && !preg_match('/{|}/', $routePath)
-        && !preg_match('/login/', $routePath)) {
+        && !preg_match('/login/', $routePath)
+        //  && preg_match('/channel/', $routePath)
+    ) {
         /* Check if we can GET (or not) */
         if (empty($routeMethod)
             || in_array('GET', $routeMethod)) {
             /* Are we in a sonata admin ? */
             if (isset($routeDefault['_controller'])
                 && array_key_exists('_sonata_admin', $routeDefault)) {
-                $curAdmin = $I->grabServiceFromContainer($routeDefault['_sonata_admin']);
+                // $curAdmin = $I->grabServiceFromContainer($routeDefault['_sonata_admin']);
                 $curMapper = null;
                 switch ($curAction) {
                     case 'list':
@@ -66,19 +104,37 @@ foreach ($curRouter->getRouteCollection() as $curRoute) {
                         break;
                 }
                 if (isset($curMapper)) {
-                    $curLabel = $curAdmin->getLabelTranslatorStrategy()->getLabel('', '', '');
-                    $I->wantTo('Test Route: ' . $routePath);
-                    $I->amOnPage($routePath);
-                    $I->waitForText('Libre', 10); // secs
-                    $I->dontSee('Stack Trace'); /* :) :) we hope so */
-                    
-                    // $libKeys = preg_grep('/^' . $curLabel . '/', array_keys($curMessage));
-                    
-                    // foreach ($libKeys as $curKeys) {
-                    //     $I->cantSeeInSource($curKeys); /* We should not see label key */
-                    // }
+                    //$curLabel = $curAdmin->getLabelTranslatorStrategy()->getLabel('', '', '');
+                    checkPage($I, $routePath, $allLink);
+                    //                    array_push($allLink, checkPage($I, $routePath));
+                
+                
+                
+                // $libKeys = preg_grep('/^' . $curLabel . '/', array_keys($curMessage));
+                
+                // foreach ($libKeys as $curKeys) {
+                //     $I->cantSeeInSource($curKeys); /* We should not see label key */
+                // }
                 }
             }
         }
     }
+}
+
+
+$uniqLink = array_unique($allLink);
+
+
+foreach ($uniqLink as $curLink) {
+    //dump($curLink);
+    $I->wantTo('Test Link: ' . $curLink);
+    $I->amOnUrl($curLink);
+    $I->waitForText('Libre', 10); // secs
+    $I->dontSee('Stack Trace'); /* :) :) we hope so */
+    
+    
+    $allLink = $I->grabMultiple('a', 'href');
+    //    $allAnchor =  preg_grep('/^#/', $allLink);
+
+    //dump($allAnchor);
 }
