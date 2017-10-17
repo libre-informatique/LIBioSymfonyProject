d2e56aac5c41f48cb8e50b589eb20c85da5064d1
François GERBES <francois.gerbes@libre-informatique.fr>
2017-10-17 11:38:42 +0200

    add some check

--- tests/acceptance/001_CnxCept.php
+++ tests/acceptance/001_CnxCept.php
@@ -13,6 +13,9 @@
 $I = new WebGuy($scenario);
 $I->wantTo('Test Login');
 $I->amOnPage('/lisem/login');
+$I->waitForText('Courriel', 30);
+$I->waitForText('Mot de passe', 30);
 $I->fillField("//input[@id='_username']", 'lisem@lisem.eu');
 $I->fillField("//input[@id='_password']", 'lisem');
 $I->click("//button[@type='submit']");
+$I->waitForText('Libre', 30);
