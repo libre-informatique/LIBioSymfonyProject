5781f816460df26b83e4e4bda4caafc708308941
François GERBES <francois.gerbes@libre-informatique.fr>
2017-10-18 18:04:25 +0200

    Switch test to dev

--- bin/ci-scripts/before_script_test.sh
+++ bin/ci-scripts/before_script_test.sh
@@ -29,7 +29,7 @@ npm run gulp
 
 
 # start server as prod for travis timeout on dev...
-bin/console server:start --no-interaction 127.0.0.1:8042 --env=prod
+bin/console server:start --no-interaction 127.0.0.1:8042 # --env=prod
 
 
 ######### TOOLS
