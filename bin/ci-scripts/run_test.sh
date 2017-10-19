40d2b999ff3bf9dc6c2954cb652a6a21d33a392a
François GERBES <francois.gerbes@libre-informatique.fr>
2017-10-19 14:55:48 +0200

    Add cool feature to debug selenium test

--- bin/ci-scripts/run_test.sh
+++ bin/ci-scripts/run_test.sh
@@ -2,23 +2,3 @@
 
 
 bin/codecept run -d --steps --fail-fast --no-interaction
-
-# try on debug
-sudo apt-get install lynx
-for i in tests/_output/*.html
-do
-    echo '========================================'
-    echo $i
-    echo '========================================'
-    lynx -dump $i
-    echo '========================================'
-done
-
-
-#if [ -e tests/_output/001_CnxCept.fail.html ]
-#then
-#    cat tests/_output/001_CnxCept.fail.html
-#fi
-
-# --no-exit
-# echo # Ah ah return 0
