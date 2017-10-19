8dc6c50ecefb81189f0668e40d6f739fdd490800
François GERBES <francois.gerbes@libre-informatique.fr>
2017-10-19 14:34:31 +0200

    try to debug output

--- bin/ci-scripts/run_test.sh
+++ bin/ci-scripts/run_test.sh
@@ -3,6 +3,18 @@
 
 bin/codecept run -d --steps --fail-fast --no-interaction
 
+# try on debug
+sudo apt-get install lynx
+for i in tests/_output/*.html
+do
+    echo '========================================'
+    echo $i
+    echo '========================================'
+    lynx -dump $i
+    echo '========================================'
+done
+
+
 #if [ -e tests/_output/001_CnxCept.fail.html ]
 #then
 #    cat tests/_output/001_CnxCept.fail.html
