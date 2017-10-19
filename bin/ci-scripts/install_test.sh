909d256e219ceef42e5aee4fd3e2712b77bd65d9
François GERBES <francois.gerbes@libre-informatique.fr>
2017-10-19 10:52:01 +0200

    Re-Organisation of build process

--- bin/ci-scripts/install_test.sh
+++ bin/ci-scripts/install_test.sh
@@ -10,19 +10,6 @@ composer require --no-interaction --dev se/selenium-server-standalone
 
 
 ######## LISEM
-# database creation
-bin/console doctrine:database:create --if-not-exists --no-interaction
-bin/console doctrine:schema:create --no-interaction
-#bin/console doctrine:schema:update --force --no-interaction
-#bin/console doctrine:schema:validate --no-interaction
-
-# lisem
-bin/console lisem:install:setup --with-samples --yes
-bin/console librinfo:patchs:apply
-bin/console assets:install
-bin/console sylius:theme:assets:install # must be done after assets:install
-
-
 #nvm use stable
 
 export NVM_DIR="$HOME/.nvm"
@@ -32,6 +19,21 @@ set -v
 
 npm install
 npm run gulp
+
+# database creation
+bin/console doctrine:schema:drop --force --no-interaction
+bin/console doctrine:database:create --if-not-exists --no-interaction
+bin/console doctrine:schema:create --no-interaction
+#bin/console doctrine:schema:update --force --no-interaction
+#bin/console doctrine:schema:validate --no-interaction
+
+# asset and data
+bin/console lisem:install:setup --with-samples --yes
+bin/console librinfo:patchs:apply
+bin/console assets:install
+bin/console sylius:theme:assets:install # must be done after assets:install
+
+
 
 
 
