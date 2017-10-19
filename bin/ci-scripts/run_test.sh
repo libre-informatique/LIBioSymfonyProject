d0a43257a7af9f1a8dee0083d148fc745a7167af
PapsOu <vivien.frasca@libre-informatique.fr>
2017-10-19 15:53:58 +0200

    Try setting custom connection for travis

--- bin/ci-scripts/run_test.sh
+++ bin/ci-scripts/run_test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
-bin/console doctrine:schema:update  --dump-sql
+bin/console doctrine:schema:update --em=default --dump-sql
+bin/console doctrine:schema:update --em=session --dump-sql
 bin/codecept run -d --steps --fail-fast --no-interaction
