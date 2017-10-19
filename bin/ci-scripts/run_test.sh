50ca9d37f480825318ea59fa6fea46147078fda8
François GERBES <francois.gerbes@libre-informatique.fr>
2017-10-19 09:38:20 +0200

    try to debug travis test

--- bin/ci-scripts/run_test.sh
+++ bin/ci-scripts/run_test.sh
@@ -2,5 +2,10 @@
 
 codecept run -d --steps --fail-fast --no-interaction
 
+if [ -e tests/_output/001_CnxCept.fail.html ]
+then
+    cat tests/_output/001_CnxCept.fail.html
+fi
+
 # --no-exit
 # echo # Ah ah return 0
