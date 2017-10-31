b64711dbc20a2e11d31beedb0b1af14fc93682c3
François GERBES <francois.gerbes@libre-informatique.fr>
2017-10-31 16:29:26 +0100

    Try to be more precise on order test

--- tests/acceptance/033_CommandeCept.php
+++ tests/acceptance/033_CommandeCept.php
@@ -75,25 +75,27 @@ function CreateCmd($webGuy)
     //$webGuy->click("Carotte nantaise");
     $webGuy->amOnPage('/products/carotte-nantaise'); /* Hum... Carotte */
     $webGuy->click("//button[@type='submit']");
-    $webGuy->wait(5);
-    $webGuy->waitForText('Paiement', 30);
-    $webGuy->click("(//a[contains(text(),'Paiement')])[2]");
+    //$webGuy->wait(5);
+    //$webGuy->waitForText('Paiement', 30);
+    $webGuy->waitForElementVisible("(//a[contains(@href, '/checkout')])[2]", 30);
+    $webGuy->click("(//a[contains(@href, '/checkout')])[2]");
+    //    $webGuy->click("(//a[contains(text(),'Paiement')])[2]");
     $webGuy->fillField('#sylius_checkout_address_shippingAddress_firstName', 'selfirst');
     $webGuy->fillField('#sylius_checkout_address_shippingAddress_lastName', 'sellast');
     $webGuy->fillField('#sylius_checkout_address_shippingAddress_street', 'sel street');
     $webGuy->fillField('#sylius_checkout_address_shippingAddress_city', 'sel city');
     $webGuy->fillField('#sylius_checkout_address_shippingAddress_postCode', '29');
-    $webGuy->scrollTo('#next-step', 100, 100);
+    $webGuy->scrollTo('#next-step');
     $webGuy->click('#next-step');
     $webGuy->waitForText('Suivant', 30);
-    $webGuy->scrollTo('#next-step', 100, 100);
+    $webGuy->scrollTo('#next-step');
     $webGuy->click('#next-step');
     $webGuy->waitForText('Suivant', 30);
-    $webGuy->scrollTo('#next-step', 100, 100);
+    $webGuy->scrollTo('#next-step');
     $webGuy->click('#next-step');
 
     $webGuy->waitForText('Récapitulatif de votre commande', 30);
-    $webGuy->scrollTo("//button[@type='submit']", 100, 100);
+    $webGuy->scrollTo("//button[@type='submit']");
     $webGuy->click("//button[@type='submit']");
     $webGuy->see('Merci ! Votre commande a bien été prise en compte.', '#sylius-thank-you');
 }
