1928a01115073289c7b694d772bae905ee8839ce
François GERBES <francois.gerbes@libre-informatique.fr>
2017-10-31 17:01:22 +0100

    Fill some method with some code ... :)

--- tests/_support/Step/Acceptance/Sylius.php
+++ tests/_support/Step/Acceptance/Sylius.php
@@ -12,18 +12,43 @@
 
 namespace Step\Acceptance;
 
-class Sylius extends \WebGuy
+class Sylius extends Common
 {
     public function createAccount()
     {
-        $I = $this;
+        $userName = $this->getRandName() . '-shop-user';
+        /* warning there is a big exception if the email is already used by someone else */
+        $userEmail = $userName . '@lisem.eu';
+
+        $this->amGoingTo('Create Shop User Account ' . $userName);
+        $this->amOnPage('/');
+        $this->click('Connexion');
+        $this->click("(//a[contains(@href, '/register')])[2]");
+        $this->fillField('#sylius_customer_registration_firstName', $userName . '-First');
+        $this->fillField('#sylius_customer_registration_lastName', $userName . '-Last');
+        $this->fillField('#sylius_customer_registration_email', $userEmail);
+        $this->fillField('#sylius_customer_registration_user_plainPassword_first', 'selpwd');
+        $this->fillField('#sylius_customer_registration_user_plainPassword_second', 'selpwd');
+        $this->click("//button[@type='submit']");
+        $this->waitForText('Succès', 10); // secs
+        return $userEmail;
     }
 
-    public function logUser()
+    public function logUser($userLogin, $userPassword = 'selpwd')
     {
-        $I = $this;
+        $sessionSnapShot = 'login_sylius_' . $username;
+        if (!$this->loadSessionSnapshot($sessionSnapShot)) {
+            $this->amGoingTo('Login Online Shop as ' . $userLogin);
+            $this->amOnPage('/');
+            $this->click('Connexion');
+            $this->fillField('#_username', $userLogin);
+            $this->fillField('#_password', $userPassword);
+            $this->click("//button[@type='submit']");
+            $this->see('Mon compte Gérer vos informations personnelles et préférences', '//h1');
+            $this->saveSessionSnapshot($sessionSnapShot);
+        }
     }
-
+    
     public function createOrder()
     {
         $I = $this;
