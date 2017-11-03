6f3d6ce56bc0a092345b39f0f3c76f028c8359eb
François GERBES <francois.gerbes@libre-informatique.fr>
2017-11-03 14:58:09 +0100

    Rollback to leftbutton

--- tests/_support/Step/Acceptance/Lisem.php
+++ tests/_support/Step/Acceptance/Lisem.php
@@ -96,12 +96,12 @@ class Lisem extends Common
         // UGLY SECOND WORKING WAY
         // $this->scrollTo('div[id^="s2id_"][id$="' . $id . '"] a', 0, -100);
         $this->clickWithLeftButton('div[id^="s2id_"][id$="' . $id . '"] a');
+
         // $this->scrollTo('//div[@id="select2-drop"]/div/input[contains(@id,"_search")]'); // is it in the footer ? moved by js ? webdriver is not aware of this move ?
         $this->waitForElementVisible('//div[@id="select2-drop"]/div/input[contains(@id,"_search")]', 30);
         $this->fillField('//div[@id="select2-drop"]/div/input[contains(@id,"_search")]', $value);
         $this->waitCube();
-        //$this->clickWithLeftButton('//div[@id="select2-drop"]/ul/li/div/div[contains(string(), "' . $value . '")]');
-        $this->click('//div[@id="select2-drop"]/ul/li/div/div[contains(string(), "' . $value . '")]');
+        $this->clickWithLeftButton('//div[@id="select2-drop"]/ul/li/div/div[contains(string(), "' . $value . '")]');
         $this->waitCube();
     }
 
@@ -109,23 +109,20 @@ class Lisem extends Common
     {
         /* @todo test if there is more than one select on the page */
         // REAL example to click select2 elements below
-        //$this->clickWithLeftButton('div[id^="s2id_"][id$="' . $id . '"] ' . $tag . '');
-        //$this->clickWithLeftButton('//div[@id="select2-drop"]/ul/li/div[text()="' . $value . '"]');
-        $this->click('div[id^="s2id_"][id$="' . $id . '"] ' . $tag . '');
-        $this->click('//div[@id="select2-drop"]/ul/li/div[text()="' . $value . '"]');
-
+        $this->clickWithLeftButton('div[id^="s2id_"][id$="' . $id . '"] ' . $tag . '');
+        /* @todo maybe move text to string */
+        $this->waitForElementVisible('//div[@id="select2-drop"]/ul/li/div[text()="' . $value . '"]');
+        $this->clickWithLeftButton('//div[@id="select2-drop"]/ul/li/div[text()="' . $value . '"]');
         $this->waitCube();
     }
 
     public function clickCheckbox($name, $value = '1')
     {
-        //$this->clickWithLeftButton('input[type="checkbox"][name$="[' . $name . ']"][value="' . $value . '"] + ins');
-        $this->click('input[type="checkbox"][name$="[' . $name . ']"][value="' . $value . '"] + ins');
+        $this->clickWithLeftButton('input[type="checkbox"][name$="[' . $name . ']"][value="' . $value . '"] + ins');
     }
 
     public function clickRadio($name, $value = '1')
     {
-        //$this->clickWithLeftButton('input[type="radio"][name$="[' . $name . ']"][value="' . $value . '"] + ins');
-        $this->click('input[type="radio"][name$="[' . $name . ']"][value="' . $value . '"] + ins');
+        $this->clickWithLeftButton('input[type="radio"][name$="[' . $name . ']"][value="' . $value . '"] + ins');
     }
 }
