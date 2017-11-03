f1b9d4da272d5cb7b5fa964f5bbf8f88f5927956
François GERBES <francois.gerbes@libre-informatique.fr>
2017-11-03 14:56:05 +0100

    Try to have more control on selenium version and driver

--- bin/ci-scripts/before_install_test.sh
+++ bin/ci-scripts/before_install_test.sh
@@ -3,23 +3,44 @@ set -ev
 
 mkdir --parents "${HOME}/bin"
 
-# Ugly hack
-echo "memory_limit=-1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
-
+if [ $# -eq 0 ]
+then
+    # Ugly hack
+    echo "memory_limit=-1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
+fi
+   
 composer self-update --stable
 composer clear-cache
 
 # for selenium
 sudo apt-get install xvfb
 sudo apt-get install chromium-browser
-sudo apt-get install chromium-chromedriver
+sudo apt-get install firefox
+#sudo apt-get install chromium-chromedriver
 
 #wget http://chromedriver.storage.googleapis.com/2.12/chromedriver_linux64.zip
 #wget http://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip
-#wget http://chromedriver.storage.googleapis.com/2.33/chromedriver_linux64.zip
-#unzip chromedriver_linux64.zip
-#mv chromedriver ${HOME}/bin/
-ln -s /usr/lib/chromium-browser/chromedriver ${HOME}/bin/chromedriver
+wget http://chromedriver.storage.googleapis.com/2.33/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+mv chromedriver ${HOME}/bin/
+#ln -s /usr/lib/chromium-browser/chromedriver ${HOME}/bin/chromedriver
+
+wget https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-linux64.tar.gz
+gunzip geckodriver-v0.19.1-linux64.tar.gz
+tar -xvf geckodriver-v0.19.1-linux64.tar
+mv geckodriver ${HOME}/bin/
+
+# selenium not need as we use a composer package for this
+#wget https://selenium-release.storage.googleapis.com/3.4/selenium-server-standalone-3.4.0.jar
+#java -jar selenium-server-standalone-3.4.0.jar &
+wget https://selenium-release.storage.googleapis.com/3.7/selenium-server-standalone-3.7.0.jar
+mv selenium-server-standalone-3.7.0.jar  ${HOME}/bin/selenium-server-standalone.jar
+
+
+# codeception not need as we use a composer package for this
+#wget "http://codeception.com/codecept.phar"  --output-document="${HOME}/bin/codecept"
+#chmod u+x "${HOME}/bin/codecept"
+
 
 
 # install nvm
@@ -36,14 +57,6 @@ set -v
 nvm install 4.2.6
 
 
-# selenium not need as we use a composer package for this
-#wget https://selenium-release.storage.googleapis.com/3.4/selenium-server-standalone-3.4.0.jar
-#java -jar selenium-server-standalone-3.4.0.jar &
-
-
-# codeception not need as we use a composer package for this
-#wget "http://codeception.com/codecept.phar"  --output-document="${HOME}/bin/codecept"
-#chmod u+x "${HOME}/bin/codecept"
 
 
 # check version
@@ -52,6 +65,7 @@ composer -V
 
 chromium-browser --version
 chromedriver --version
+geckodriver --version
 
 nvm --version
 npm -v
--- bin/ci-scripts/run_test.sh
+++ bin/ci-scripts/run_test.sh
@@ -6,19 +6,21 @@ OUTPUTDIR=tests/_output
 rm -rf $OUTPUTDIR/*.png
 rm -rf $OUTPUTDIR/*.html
 
-CODECEPTCMD="bin/codecept run -d --steps --fail-fast --no-interaction --no-exit --env=chrome"
+CODECEPTCMD="bin/codecept run -d --steps --fail-fast --no-interaction --no-exit"
+
 
 CODECEPTGROUP=$@
 if [ $# -eq 0 ]
 then
-   CODECEPTGROUP="login menu user crm variety ecommerce" 
+   CODECEPTGROUP="login menu user crm" # variety ecommerce" # all"
 fi
 
 
 
 for i in $CODECEPTGROUP
 do
-    $CODECEPTCMD -g $i
+    # $CODECEPTCMD -g $i --env=firefox
+    $CODECEPTCMD -g $i --env=chrome
 done
 
 
