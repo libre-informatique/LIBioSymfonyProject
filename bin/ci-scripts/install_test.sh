500024275eb314092d1fd24f81b5e9b580058168
François GERBES <francois.gerbes@libre-informatique.fr>
2018-01-10 10:43:28 +0100

    [CI] Rerun script on composer install

--- bin/ci-scripts/install_test.sh
+++ bin/ci-scripts/install_test.sh
@@ -4,7 +4,7 @@ set -ev
 rm -rf composer.lock # vendor
 
 # composer update --no-interaction --prefer-dist
-composer install --no-scripts --no-interaction --prefer-dist
+composer install --no-interaction --prefer-dist
 
 
 #composer require --no-interaction --dev phpunit/phpunit
