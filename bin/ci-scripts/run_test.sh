d2c6101b07a44a7468402557840c141340b8dde2
PapsOu <vivien.frasca@libre-informatique.fr>
2018-01-11 17:38:41 +0100

    Fix all tests

--- bin/ci-scripts/run_test.sh
+++ bin/ci-scripts/run_test.sh
@@ -16,7 +16,7 @@ CODECEPTCMD="bin/codecept run -vvv --debug --steps --no-interaction"
 CODECEPTGROUP=$@
 if [ $# -eq 0 ]
 then
-    CODECEPTGROUP="login menu user crm" # variety ecommerce" # all"
+    CODECEPTGROUP="login menu user crm variety ecommerce" # all"
 fi
 
 for i in $CODECEPTGROUP
@@ -25,7 +25,7 @@ do
     if [ $i = 'route' ]
     then
         $CODECEPTCMD --no-exit -g route --env=phpbrowser,lisem
-    else        
+    else
         # merge env http://codeception.com/docs/07-AdvancedUsage#Environments
         $CODECEPTCMD --fail-fast -g $i --env=firefox,lisem
         #$CODECEPTCMD -g $i --env=chrome,lisem
